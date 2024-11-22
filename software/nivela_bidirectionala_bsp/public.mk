cd275dd14ac15c52e8bd42d68351a919269db121
Razvan Rujoi <razvanrujoi@yahoo.com>
2024-11-22 23:36:43 +0200

    feature: update bsp files

--- software/nivela_bidirectionala_bsp/public.mk
+++ software/nivela_bidirectionala_bsp/public.mk
@@ -158,9 +158,9 @@ SOPC_SYSID_FLAG += --sidp=0x41070
 ELF_PATCH_FLAG  += --sidp 0x41070
 
 # The SOPC Timestamp 
-# setting SOPC_TIMESTAMP is 1732141602
-SOPC_SYSID_FLAG += --timestamp=1732141602
-ELF_PATCH_FLAG  += --timestamp 1732141602
+# setting SOPC_TIMESTAMP is 1732203005
+SOPC_SYSID_FLAG += --timestamp=1732203005
+ELF_PATCH_FLAG  += --timestamp 1732203005
 
 # Enable JTAG UART driver to recover when host is inactive causing buffer to 
 # full without returning error. Printf will not fail with this recovery. none 
