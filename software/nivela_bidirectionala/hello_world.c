422d2c83768f74a34fd5d644747dd089f419810c
Razvan Rujoi <razvanrujoi@yahoo.com>
2024-11-22 23:35:10 +0200

    feature: create application for release 1.0

--- software/nivela_bidirectionala/hello_world.c
+++ software/nivela_bidirectionala/hello_world.c
@@ -1,21 +1,38 @@
 #include "hex_displays.h"
 #include "accelerometer.h"
-#include <system.h>
+#include "global.h"
+#include <unistd.h>
 #include <altera_avalon_pio_regs.h>
+#include <altera_up_avalon_accelerometer_spi.h>
 
-#define UP_LEDS 1
-#define DOWN_LEDS 0
-
-#define TURN_OFF 0xFFFF
-#define TURN_ON ~TURN_OFF
+alt_up_accelerometer_spi_dev* accelerometer_device = NULL;
 
 int main()
 {
 	printf("Program start\r\n");
 	hex_init(TURN_OFF);
-	accelerometer_init();
+
+	accelerometer_device = alt_up_accelerometer_spi_open_dev("/dev/accelerometer_spi_0");
+	read_ADXL345_deviceID(accelerometer_device);
+
+	alt_32 y;
+	alt_32 y_inclination = 150;
 	while(1)
 	{
+		alt_up_accelerometer_spi_read_y_axis(accelerometer_device, &y);
+		if(y > y_inclination)
+		{
+			set_hex_circle(DOWN_LEDS, 0);
+		}
+		else if (y < (-y_inclination))
+		{
+			set_hex_circle(UP_LEDS, 0);
+		}
+		else
+		{
+			set_hex_state(1, 0);
+		}
+		usleep(10000);
 	}
 
     return 0;
