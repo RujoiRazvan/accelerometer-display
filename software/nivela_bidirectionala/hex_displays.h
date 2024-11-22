b85b46c686f2396649e3aac1bbb58987d274f8d6
Razvan Rujoi <razvanrujoi@yahoo.com>
2024-11-22 23:32:46 +0200

    feat: create hex display set state function

--- software/nivela_bidirectionala/hex_displays.h
+++ software/nivela_bidirectionala/hex_displays.h
@@ -1,6 +1,9 @@
-#include <stdio.h>
+#include "global.h"
 #include <alt_types.h>
 #include <altera_avalon_pio_regs.h>
 
 void hex_init (uint32_t hexState);
+
 void set_hex_circle(alt_u8 position, alt_u8 hex_index);
+
+void set_hex_state (alt_u8 state, alt_u8 hex_index);
