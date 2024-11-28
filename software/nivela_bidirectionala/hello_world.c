a42ab51d64605d83ad0de9e5443dd11166f0dbfe
Razvan Rujoi <razvanrujoi@yahoo.com>
2024-11-28 19:17:24 +0200

    chore: change hex functions appends

--- software/nivela_bidirectionala/hello_world.c
+++ software/nivela_bidirectionala/hello_world.c
@@ -15,22 +15,23 @@ int main()
 	accelerometer_device = alt_up_accelerometer_spi_open_dev(ACCELEROMETER_SPI_0_NAME);
 	read_ADXL345_deviceID(accelerometer_device);
 
-	alt_32 y;
+	alt_32 y = 512, x;
 	alt_32 y_inclination = 150;
 	while(1)
 	{
+		alt_up_accelerometer_spi_read_x_axis(accelerometer_device, &x);
 		alt_up_accelerometer_spi_read_y_axis(accelerometer_device, &y);
 		if(y > y_inclination)
 		{
-			set_hex_circle(DOWN_LEDS, 0);
+			set_hex_value(TURN_ON_DOWN_LEDS, 0);
 		}
 		else if (y < (-y_inclination))
 		{
-			set_hex_circle(UP_LEDS, 0);
+			set_hex_value(TURN_ON_UP_LEDS, 0);
 		}
 		else
 		{
-			set_hex_state(1, 0);
+			set_hex_value (TURN_ON_MIDDLE_LINE, 0);
 		}
 		usleep(10000);
 	}
