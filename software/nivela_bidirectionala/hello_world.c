3fb78deeac9780e2820a3cd563503f88fab5bf2a
Razvan Rujoi <razvanrujoi@yahoo.com>
2024-12-03 00:22:10 +0200

    refactor: fix hex display turn on algorithm

--- software/nivela_bidirectionala/hello_world.c
+++ software/nivela_bidirectionala/hello_world.c
@@ -15,24 +15,25 @@ int main()
 	accelerometer_device = alt_up_accelerometer_spi_open_dev(ACCELEROMETER_SPI_0_NAME);
 	read_ADXL345_deviceID(accelerometer_device);
 
-	alt_32 y = 512, x;
-	alt_32 y_inclination = 150;
+	alt_32 y = 512, x = 512;
+	int state = 0;
 	while(1)
 	{
 		alt_up_accelerometer_spi_read_x_axis(accelerometer_device, &x);
 		alt_up_accelerometer_spi_read_y_axis(accelerometer_device, &y);
-		if(y > y_inclination)
-		{
-			set_hex_value(TURN_ON_DOWN_LEDS, 0);
-		}
-		else if (y < (-y_inclination))
-		{
-			set_hex_value(TURN_ON_UP_LEDS, 0);
-		}
+		int index = (x + 255) / 86;
+		x = (index < 0) ? 0 : (index >= 6) ? 5 : index;
+		int current_state = hysteresis(y, &state);
+
+		if (current_state == 1)
+			set_hex_value(TURN_ON_DOWN_LEDS, x);
+		else if (current_state == -1)
+			set_hex_value(TURN_ON_UP_LEDS, x);
 		else
-		{
-			set_hex_value (TURN_ON_MIDDLE_LINE, 0);
-		}
+			set_hex_value(TURN_ON_MIDDLE_LINE, x);
+
+		for (int i = 0; i < 6; i++) { if (i != x) { set_hex_value(0xFF, i); } };
+
 		usleep(10000);
 	}
 
