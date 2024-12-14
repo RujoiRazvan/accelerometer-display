fc6ce9475228a7c41249a67258da4229c1840879
Razvan Rujoi <razvanrujoi@yahoo.com>
2024-12-14 22:26:31 +0200

    feat(source): add source files for adc peripheral

---
+++ software/nivela_bidirectionala/adc.h
@@ -0,0 +1,15 @@
+/*
+ * adc.h
+ *
+ *  Created on: Dec 14, 2024
+ *      Author: razva
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include "global.h"
+
+unsigned int getAdcValue(alt_up_adc_dev * adc);
+
+#endif /* ADC_H_ */
